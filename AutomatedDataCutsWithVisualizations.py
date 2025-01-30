4f6f4b61a759c42270e5dfafbe32afce6ab7ceef
qciosek <quincyciosek@gmail.com>
2025-01-30 11:42:36 -0500

    Update AutomatedDataCutsWithVisualizations.py

--- AutomatedDataCutsWithVisualizations.py
+++ AutomatedDataCutsWithVisualizations.py
@@ -59,7 +59,9 @@ connection = connect_to_db()
 # Clear Streamlit cache
 st.cache_data.clear()
 # Fetch data and sample size
+@st.cache_data(ttl=0)  # Forces Streamlit to re-fetch data every time
 def fetch_data_and_sample_size(connection, selected_questions):
+
     question_code_filter = "', '".join(selected_questions)
     if question_code_filter:
         # Calculate the sample size: Participants who said "Yes" to all selected questions
