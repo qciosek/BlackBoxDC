92c06395960dd13eba1d9a2eadef75eb0b29181a
qciosek <quincyciosek@gmail.com>
2025-02-07 11:14:17 -0500

    Update AutomatedDataCutsWithVisualizations.py

--- AutomatedDataCutsWithVisualizations.py
+++ AutomatedDataCutsWithVisualizations.py
@@ -275,6 +275,8 @@ def main():
     question_df_all = pd.read_sql(question_query_all, connection)
 
     question_df['dropdown_label'] = question_df['answer_text'] + ", " + question_df['question_code'] + ", " + question_df['question_text']
+    question_df_all['dropdown_label'] = question_df_all['answer_text'] + ", " + question_df_all['question_code'] + ", " + question_df_all['question_text']
+
     question_options = ["No Answer"] + question_df['dropdown_label'].tolist()
 
     question_selected_1 = st.selectbox("Select a Question (Optional):", question_options)
