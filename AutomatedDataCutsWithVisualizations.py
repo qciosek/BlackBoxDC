40bae18d4517f618a6430a7a5fa25a3c8c758f8d
qciosek <quincyciosek@gmail.com>
2025-09-10 11:50:11 -0400

    Update AutomatedDataCutsWithVisualizations.py

--- AutomatedDataCutsWithVisualizations.py
+++ AutomatedDataCutsWithVisualizations.py
@@ -28,8 +28,8 @@ elif dataset_option == "Linear TV Study":
     responses_table = "responses_4"
     question_mapping_table = "question_mapping_4"
 elif dataset_option == "Drivers of Sports Fandom (new)"
-    responses_table = 'responses_5'
-    question_mapping_table = 'question_mapping_5'
+    responses_table = "responses_5"
+    question_mapping_table = "question_mapping_5"
 else:  # Test Dataset
     responses_table = "responses_3"
     question_mapping_table = "question_mapping_3"
