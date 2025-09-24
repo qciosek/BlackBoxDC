ad8a74b8fd0ddc07df7ae3d57f3af45a011c631c
qciosek <quincyciosek@gmail.com>
2025-09-24 13:34:18 -0400

    Update AutomatedDataCutsWithVisualizations.py

--- AutomatedDataCutsWithVisualizations.py
+++ AutomatedDataCutsWithVisualizations.py
@@ -289,7 +289,7 @@ def main():
     st.markdown(
     """
     <div style="text-align: center;">
-        <h1 style="font-size: 38px; margin: 0; padding: 0;">Black Box Data</h1>
+        <h1 style="font-size: 38px; margin: 0; padding: 0;">Green Light IQ</h1>
         <h2 style="font-size: 28px; margin: 0; padding: 0;">
             <em>"The Smartest Way to Power Your Content Engine"</em> 
         </h2>
